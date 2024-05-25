019cd5e0f4f07ae94a87d763595857af7f7963c8
Joy <kweyuayieko@gmail.com>
2024-05-25 21:44:10 +0300

    Feat:Completed attributes and properties lab

--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,43 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
-    pass
+    def __init__(self, name="John Doe", age=30, job="General Management"):
+        self.name = name
+        self.age = age
+        self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self, age):
+        if isinstance(age, (int, float)) and 0 <= age <= 120:
+            self._age = age
+        else:
+            print("Age must be a number between 0 and 120.")
+
+    age = property(get_age, set_age)
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+    
